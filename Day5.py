5033660f60f1a186a24a349ca7f96b3a8559e7f0
dv6-win10 <behappy@plusdica.com>
2016-11-28 21:46:53 -0500

    Add works from Day5.

---
+++ Day5.py
@@ -0,0 +1,50 @@
+#
+# from fibo_root import *
+#
+# fib2(10)
+# fib(10)
+
+
+# from pyFibo.fibo import fib
+# fib(10)
+
+# from fibo_root import fib2
+# fib2(10)
+
+# from pyFibo import fibo
+# fibo.fib2()
+
+# from fibo_root import *
+# fib()
+
+
+import tkinter.test.test_tkinter.test_font
+
+
+# import os
+# from pyFibo import fibo
+# from pyFibo.fibo import *
+#
+# import fibo_root
+#
+#
+# print(os.getcwd())
+# os.chdir("C:\\")
+# print(os.getcwd())
+#
+# print("from root folder")
+# fibo_root.fib(10)
+# print(fibo_root.fib2(10))
+#
+# print("")
+# print("from sub-folder")
+# fibo.fib(10)
+# print(fibo.fib2(10))
+#
+# print("")
+# odd.even()
+# print("")
+# help("pybo_root.fib")
+
+
+
